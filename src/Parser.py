1f45ffa02d8f65716a170a3f20297ea47abebf5d
Salmane Bah <salmane.bah@ensimag.grenoble-inp.fr>
2015-11-07 14:01:51 +0100

    Parser: Add Task class

--- src/Parser.py
+++ src/Parser.py
@@ -13,4 +13,24 @@ class ParseError(Exception):
         self.msg = msg
 
     
+class Task:
+
+    def __init__(self, debug_id = -1):
+        self.target = None
+        self.dependencies = set()
+        self.command = None
+        self.state = State.WAITING
+
+    def __hash__(self):
+        return self.target.__hash__()
+
+    def __eq__(self, other):
+        return (isinstance(other, self.__class_)) and self.target == other.target
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def get_debug_node(self):
+        return "node" + str(self_id)
+
     

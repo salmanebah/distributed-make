80a26401659d7e71e4d47ff03d85e43f1c89dfce
Salmane Bah <salmane.bah@ensimag.grenoble-inp.fr>
2015-11-07 14:02:33 +0100

    Parser: Add Parser class

--- src/Parser.py
+++ src/Parser.py
@@ -20,6 +20,7 @@ class Task:
         self.dependencies = set()
         self.command = None
         self.state = State.WAITING
+        self._id = debug_id # for debugging purpose
 
     def __hash__(self):
         return self.target.__hash__()
@@ -33,4 +34,48 @@ class Task:
     def get_debug_node(self):
         return "node" + str(self_id)
 
+class Parser:
+    _TARGET_START = '`'
+    _TARGET_END = '\''
+    _TARGET_START_LINE = 'Considering target file'
+    _TARGET_REMAKE_LINE = 'Must remake target '
+    _TARGET_PRUNE_LINE = 'Pruning file '
+    _TARGET_END_LINE = 'Finished prerequisites of target file '
+
+    def __init__(self):
+        self._target_to_task = {}
+        
+    def _get_task_from_target(self, target):
+        if target in self._target_to_task:
+            return self._target_to_task[target]
+        else:
+            debug_id = len(self._target_to_task) + 1
+            task = Task(debug_id)
+            self._target_to_task[target] = task
+            return task
+
+    def _build_dependencies_tree(current_task_node):
+        for line in fileinput.input():
+            trimed = line.strip()
+        
+    def parse_makefile(self):
+        pass
+
+    def sort_tasks(self):
+        pass
+
+    def dependencies_tree_to_dot(self):
+        pass
+        
+
+def _extract_target_name(line):
+    start_index = line.find(_TARGET_START) + 1
+    end_index = line.find(_TARGET_END)
+    return line[start_index : end_index]
+
+
+
+
+
+# class TaskCluster:
     

72da289ff6bedb2f4a7f4ac1360a92e6fe8223cf
Salmane Bah <salmane.bah@ensimag.grenoble-inp.fr>
2015-11-25 02:25:29 +0100

    Parser: Add Unit test for Makefile in blender_2.49

---
+++ test/test_blender_249.py
@@ -0,0 +1,75 @@
+"""
+module to test the parsing of the Mafile in makefiles/blender_2.49
+"""
+
+import sys
+import re
+import unittest
+
+sys.path.append('../src')
+
+from Parser import Parser
+
+class Blender249TestCase(unittest.TestCase):
+    """
+    Test case for Makefile in makefiles/blender_2.49.
+    """
+    def setUp(self):
+        """Setup the test case."""
+        self.makefile = open('makefiles/blender_2.49/Makefile')
+
+    def tearDown(self):
+        """ Tear down the test case."""
+        self.makefile.close()
+
+    def test_cube_is_target(self):
+        """ Test that cube.mpg is valid target in the makefile
+            with the expected command and dependencies."""
+        parser = Parser()
+        parser.parse_makefile(self.makefile)
+        tasks = parser.get_sorted_tasks()
+        # raises StopIteration if not found
+        cube_task = next(task for task in tasks if task.target == 'cube.mpg')
+        command_parts = cube_task.command.split()
+        dependency_names = [dep.target for dep in cube_task.dependencies]
+        self.assertEquals(len(command_parts), 115)
+        self.assertTrue(all((lambda part:
+                             part == 'convert' or part == 'cube.mpg'
+                             or re.match('frame_[0-9]+\.png', part)) \
+                             (curr_part) for curr_part in command_parts))
+        self.assertEquals(len(dependency_names), 113)
+        self.assertTrue(all((lambda name: re.match('frame_[0-9]+\.png', name) \
+                             (curr_name) for curr_name in dependency_names)))
+
+    def test_cub_anim_target(self):
+        """ Test that cube_anim.blend is a valid target,
+             is not a file dependency and check its command."""
+        parser = Parser()
+        parser.parse_makefile(self.makefile)
+        tasks = parser.get_sorted_tasks()
+        cube_anim_task = next(task for task in tasks
+                              if task.target == 'cube_anim.blend')
+        self.assertFalse(cube_anim_task.is_file_dependency())
+        self.assertEquals(cube_anim_task.command,
+                          'rm cube_anim.blend ; unzip cube_anim.zip')
+
+    def test_other_target_are_frame(self):
+        """ Test that target other than cube.png and cube_anim.blend
+            are all frame_[0-9]+.png, have cube_anim.blend as dependency
+            and their command starts with blender."""
+        parser = Parser()
+        parser.parse_makefile(self.makefile)
+        tasks = parser.get_sorted_tasks()
+        frame_tasks = [task for task in tasks
+                       if task.target.startswith('frame')]
+        names = [task.target for task in frame_tasks]
+        commands = [task.command for task in frame_tasks]
+        dependencies = [task.dependencies for task in frame_tasks]
+        self.assertEquals(len(names), 113)
+        self.assertTrue(all((lambda name: re.match('frame_[0-9]+\.png', name) \
+                            (curr_name) for curr_name in names)))
+        self.assertTrue(all((lambda dep: len(dep) == 1
+                            and dep[0].target == 'cube_anim.blend') \
+                            (curr_dep) for curr_dep in dependencies))
+        self.assertTrue(all((lambda cmd: cmd.startswith('blender')) \
+                            (curr_cmd) for curr_cmd in commands))

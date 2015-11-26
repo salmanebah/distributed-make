cd4aff3bc8e190f712217744cb16266721c78f74
Salmane Bah <salmane.bah@ensimag.grenoble-inp.fr>
2015-11-26 22:49:47 +0100

    Deploy: Update deploy script
    Add usage help, read taktuk script and puppet manifest from the
    command line

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,5 +1,10 @@
 #! /usr/bin/env bash
 
+if [ $# -lt 2 ]
+then
+    printf 'help: %s <taktuk_script> <puppet_manifest> ' $(basename $0)
+    exit 1
+fi
 #get the master node
 uniq $OAR_NODE_FILE | head -n 1 > master_node
 # get the workers node
@@ -9,4 +14,4 @@ uniq $OAR_NODE_FILE | tail -n +2 > worker_nodes
 kadeploy3 -f $OAR_NODE_FILE -e wheezy-x64-nfs -k
 
 # install puppet on nodes and execute recipes
-taktuk -l root -s -f $OAR_NODE_FILE broadcast exec [ /home/sbah/public/dmake/taktuk_cmd.sh ]
+taktuk -l root -s -f $OAR_NODE_FILE broadcast exec [ $1 $2 ]

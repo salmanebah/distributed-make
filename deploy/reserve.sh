3ce0d8f454f30e689076d82c6b10636f51fdd082
Salmane Bah <salmane.bah@ensimag.grenoble-inp.fr>
2015-11-26 22:52:18 +0100

    Deploy: Update reserve script
    Add usage help

--- deploy/reserve.sh
+++ deploy/reserve.sh
@@ -1,3 +1,9 @@
 #! /usr/bin/env bash
 
-oarsub  -l nodes=$1,walltime=$2 -t deploy /home/sbah/public/dmake/deploy.sh
+if [ $# -lt 2 ]
+then
+    printf 'help: %s <NR_NODES> <WALLTIME_SPEC> ' $(basename $0)
+    exit 1
+fi
+
+oarsub  -I -l nodes=$1,walltime=$2 -t deploy 

24958d911041d8546520d8e5deaffd70e2891efd
Quentin Bouget <quentin.bouget.ocre@cea.fr>
2015-12-01 12:13:33 +0100

    Add: ref to "install.pp" and "taktuk_cmd.sh" semi-hard-coded

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,10 +1,9 @@
 #! /usr/bin/env bash
 
-if [ $# -lt 2 ]
-then
-    printf 'help: %s <taktuk_script> <puppet_manifest> ' $(basename $0)
-    exit 1
-fi
+PWD="`(cd $(dirname \"$0\") && pwd )`"
+TAKTUK="${PWD}/taktuk_cmd.sh"
+MANIFEST="${PWD}/install.pp"
+
 #get the master node
 uniq $OAR_NODE_FILE | head -n 1 > master_node
 # get the workers node
@@ -14,4 +13,4 @@ uniq $OAR_NODE_FILE | tail -n +2 > worker_nodes
 kadeploy3 -f $OAR_NODE_FILE -e jessie-x64-nfs -k
 
 # install puppet on nodes and execute recipes
-taktuk -l root -s -f $OAR_NODE_FILE broadcast exec [ $1 $2 ]
+taktuk -l root -s -f $OAR_NODE_FILE broadcast exec [ $TAKTUK $MANIFEST ]

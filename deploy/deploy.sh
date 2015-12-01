ade0531449eacda1b84d7339122a14e8b8bc3d81
Salmane Bah <salmane.bah@ensimag.grenoble-inp.fr>
2015-12-01 01:13:51 +0100

    Deploy: Change the deployment image from jessie

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -11,7 +11,7 @@ uniq $OAR_NODE_FILE | head -n 1 > master_node
 uniq $OAR_NODE_FILE | tail -n +2 > worker_nodes
 
 # deploy
-kadeploy3 -f $OAR_NODE_FILE -e wheezy-x64-nfs -k
+kadeploy3 -f $OAR_NODE_FILE -e jessie-x64-nfs -k
 
 # install puppet on nodes and execute recipes
 taktuk -l root -s -f $OAR_NODE_FILE broadcast exec [ $1 $2 ]

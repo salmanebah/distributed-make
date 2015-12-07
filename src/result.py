94a6dc54f70bbf7cde4e9f1461b8668cba418886
Quentin Bouget <quentin.bouget.ocre@cea.fr>
2015-12-08 00:20:52 +0100

    Ref: pylint on result.py

--- src/result.py
+++ src/result.py
@@ -3,10 +3,22 @@
 # this file is part of the SDCA project (team 11)
 #
 
+"""
+This module allows a user to fetch the last execution time of a
+makefile
+
+Please make sure the execution actually ended before running this
+"""
+
 from work import RED, START_TIME, END_TIME
 import sys
 
 def main(result_file):
+    """
+    Appends the last execution time of a makefile to ``result_file``
+
+    Please make sure the execution actually ended before running this
+    """
     with open(result_file, 'a+') as stream:
         duration = float(RED.get(END_TIME)) - float(RED.get(START_TIME))
         stream.write(str(duration) + "\n")
